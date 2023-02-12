e9f8ef71889ea0ac4f5f497b75974b1947112c86
Jorge Luis Hernandez Dueñas <jorgelhd94@gmail.com>
2023-02-12 11:02:55 -0500

    fix github link

--- src/components/atoms/Links/GithubLink/GithubLink.tsx
+++ src/components/atoms/Links/GithubLink/GithubLink.tsx
@@ -5,7 +5,7 @@ import { faGithub } from "@fortawesome/free-brands-svg-icons";
 const GithubLink = () => {
   return (
     <a
-      href="https://github.com/jorgelhd94/gateways-managment"
+      href="https://github.com/jorgelhd94"
       target="_blank"
       className="flex px-2 py-1 items-center rounded-full text-gray-400 hover:text-gray-700 bg-white shadow text-md"
     >
