dd9fcf4692e41d44ac1094bf97c95c1b502dac2f
Jorge Luis Hernandez Dueñas <jorgelhd94@gmail.com>
2023-02-13 15:49:57 -0500

    Add table skeleton

--- src/components/molecules/Gateways/GatewayTable/GatewayTable.tsx
+++ src/components/molecules/Gateways/GatewayTable/GatewayTable.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import EmptyList from "../../../atoms/Messages/EmptyList/EmptyList";
 import FetchError from "../../../atoms/Messages/FetchError/FetchError";
+import TableSkeleton from "../../../atoms/TableSkeleton/TableSkeleton";
 
 const GatewayTable = () => {
   return (
     <div>
       {/* <EmptyList /> */}
-      <FetchError />
+      {/* <FetchError /> */}
+      {/* <TableSkeleton/> */}
     </div>
   );
 };
