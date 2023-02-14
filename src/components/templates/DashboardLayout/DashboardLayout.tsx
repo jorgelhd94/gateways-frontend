154b264fdbeaee96e8f7236f896e36b88c702fcd
Jorge Luis Hernandez Dueñas <jorgelhd94@gmail.com>
2023-02-14 08:31:10 -0500

    add skeleton to dashboard

--- src/components/templates/DashboardLayout/DashboardLayout.tsx
+++ src/components/templates/DashboardLayout/DashboardLayout.tsx
@@ -1,10 +1,11 @@
 import { ReactNode } from "react";
-import LoadingScreen from "../../atoms/preloaders/LoadingScreen/LoadingScreen";
 import HeaderDashboard from "../../organisms/HeaderDashboard/HeaderDashboard";
 import SidebarDashboard from "../../molecules/SidebarDashboard/SidebarDashboard";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
+import TableSkeleton from "../../atoms/Table/TableSkeleton/TableSkeleton";
 
 const DashboardLayout = () => {
+  const navigation = useNavigation();
   return (
     <main className="bg-gray-100 h-screen overflow-hidden relative">
       <div className="flex items-start justify-between">
@@ -12,7 +13,7 @@ const DashboardLayout = () => {
         <div className="flex flex-col w-full md:space-y-4">
           <HeaderDashboard />
           <div className="overflow-y-scroll h-screen pt-4 pb-32">
-            <Outlet />
+            {navigation.state === "loading" ? <TableSkeleton /> : <Outlet />}
           </div>
         </div>
       </div>
