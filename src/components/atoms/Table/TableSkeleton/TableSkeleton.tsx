89b11f15758afccb0d1ca097eeac957ee032ed1f
Jorge Luis Hernandez Dueñas <jorgelhd94@gmail.com>
2023-02-15 17:34:00 -0500

    fixing skeleton

--- src/components/atoms/Table/TableSkeleton/TableSkeleton.tsx
+++ src/components/atoms/Table/TableSkeleton/TableSkeleton.tsx
@@ -3,27 +3,29 @@ import CardContainer from "../../CardContainer/CardContainer";
 
 const TableSkeleton = () => {
   return (
-    <CardContainer>
-      <div className="bg-white w-full mx-auto rounded-2xl mt-8">
-        <div className="bg-gray-200 h-12 p-3 overflow-hidden animate-pulse"></div>
-        <div className="">
-          <div className="grid grid-cols-3 gap-4 mt-2">
-            <div className="h-8 bg-gray-200 rounded animate-pulse"></div>
-            <div className="h-8 bg-gray-200 rounded animate-pulse"></div>
-            <div className="h-8 bg-gray-200 rounded animate-pulse"></div>
-            <div className="h-8 bg-gray-200 rounded animate-pulse"></div>
-            <div className="h-8 bg-gray-200 rounded animate-pulse"></div>
-            <div className="h-8 bg-gray-200 rounded animate-pulse"></div>
-            <div className="h-8 bg-gray-200 rounded animate-pulse"></div>
-            <div className="h-8 bg-gray-200 rounded animate-pulse"></div>
-            <div className="h-8 bg-gray-200 rounded animate-pulse"></div>
-            <div className="h-8 bg-gray-200 rounded animate-pulse"></div>
-            <div className="h-8 bg-gray-200 rounded animate-pulse"></div>
-            <div className="h-8 bg-gray-200 rounded animate-pulse"></div>
+    <div className="mx-4">
+      <CardContainer>
+        <div className="bg-white w-full mx-auto rounded-2xl mt-8">
+          <div className="bg-gray-200 h-12 p-3 overflow-hidden animate-pulse"></div>
+          <div className="">
+            <div className="grid grid-cols-3 gap-4 mt-2">
+              <div className="h-8 bg-gray-200 rounded animate-pulse"></div>
+              <div className="h-8 bg-gray-200 rounded animate-pulse"></div>
+              <div className="h-8 bg-gray-200 rounded animate-pulse"></div>
+              <div className="h-8 bg-gray-200 rounded animate-pulse"></div>
+              <div className="h-8 bg-gray-200 rounded animate-pulse"></div>
+              <div className="h-8 bg-gray-200 rounded animate-pulse"></div>
+              <div className="h-8 bg-gray-200 rounded animate-pulse"></div>
+              <div className="h-8 bg-gray-200 rounded animate-pulse"></div>
+              <div className="h-8 bg-gray-200 rounded animate-pulse"></div>
+              <div className="h-8 bg-gray-200 rounded animate-pulse"></div>
+              <div className="h-8 bg-gray-200 rounded animate-pulse"></div>
+              <div className="h-8 bg-gray-200 rounded animate-pulse"></div>
+            </div>
           </div>
         </div>
-      </div>
-    </CardContainer>
+      </CardContainer>
+    </div>
   );
 };
 
