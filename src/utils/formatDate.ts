04983bf145679ef64f78b1322134135a342a811c
Jorge Luis Hernandez Dueñas <jorgelhd94@gmail.com>
2023-02-17 14:22:14 -0500

    finish tests

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,14 +1,17 @@
-const formatDate = function (date: Date) {
-  const dateObj = new Date(date);
-  let day = "" + dateObj.getDate();
-  let month = "" + (dateObj.getMonth() + 1);
-  const year = dateObj.getFullYear();
+const formatDate = (date: Date): string => {
+  let day: string = String(date.getDate());
+  let month: string = String(date.getMonth());
+  const year: number = date.getFullYear();
 
-  if (month.length < 2) month = "0" + month;
-  if (day.length < 2) day = "0" + day;
+  if (month.length < 2) {
+    month = "0" + month;
+  }
+  if (day.length < 2) {
+    day = "0" + day;
+  }
 
-  const formater = day + "-" + month + "-" + year;
-  return formater;
+  const formatter: string = `${day}-${month}-${year}`;
+  return formatter;
 };
 
 export default formatDate;
